1b2a8e925e7b9d220405d9b196f7fb8a66ac03c1
Patrice PETIT <patricepetit@hotmail.com>
2018-10-19 20:36:57 +0200

    Core: remove commented code

--- dist/main.js
+++ dist/main.js
@@ -42,14 +42,7 @@ function _unlinkOnlyFile(file, callback) {
         }
     });
 }
-// function __awaiter(thisArg, _arguments, P, generator) {
-//     return new (P || (P = Promise))(function (resolve, reject) {
-//         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-//         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-//         function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-//         step((generator = generator.apply(thisArg, _arguments || [])).next());
-//     });
-// }
+
 class HttpsLinksConverter {
     constructor(folder) {
         if (!folder) {
--- src/main.ts
+++ src/main.ts
@@ -30,14 +30,6 @@ function _unlinkOnlyFile (file: string, callback: (err: NodeJS.ErrnoException) =
 
 }
 
-// function __awaiter(thisArg, _arguments, P, generator) {
-//     return new (P || (P = Promise))(function (resolve, reject) {
-//         function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-//         function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-//         function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-//         step((generator = generator.apply(thisArg, _arguments || [])).next());
-//     });
-// }
 export default class HttpsLinksConverter {
 
 	private folder: string;
